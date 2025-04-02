44ba172495e2576ee5f76ca4bb71a2c37d5fcb14
dremor12 <dorian1995andr@yandex.ru>
2025-04-02 11:59:12 +0300

    Выполнена доработка по избежаний гонки в OAuth2Service

--- ImageFeed/Auth/OAuth2Service.swift
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -16,20 +16,14 @@ final class OAuth2Service {
     func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
         assert(Thread.isMainThread)
         
-        if task != nil {
-            if lastCode != code {
-                task?.cancel()
-            } else {
-                completion(.failure(AuthServiceError.invalidRequest))
-                return
-            }
-        } else {
-            if lastCode == code {
-                completion(.failure(AuthServiceError.invalidRequest))
-                return
-            }
+        guard lastCode != code else {
+            completion(.failure(AuthServiceError.invalidRequest))
+            return
         }
+        
+        task?.cancel()
         lastCode = code
+        
         guard
             let request = makeOAuthTokenRequest(code: code)
         else {
@@ -47,7 +41,7 @@ final class OAuth2Service {
                         self?.tokenStorage.token = responseBody.accessToken
                         completion(.success(responseBody.accessToken))
                     } catch {
-                        print("Ошибка декодирования JSON: \(error.localizedDescription)")
+                        assertionFailure("Ошибка декодирования JSON: \(error.localizedDescription)")
                         completion(.failure(error))
                     }
                 } else {
@@ -84,7 +78,7 @@ final class OAuth2Service {
         do {
             request.httpBody = try JSONSerialization.data(withJSONObject: bodyParams, options: [])
         } catch {
-            print("[OAuth2Service]: JSONSerialization Error - \(error.localizedDescription)")
+            assertionFailure("[OAuth2Service]: JSONSerialization Error - \(error.localizedDescription)")
             return nil
         }
         return request
